371d9f14272b0be44f9605ece11260f30a902afc
marciofreire42 <c135532@gmail.com>
2022-01-09 13:39:59 -0300

    Adicionado Controlador dos campos de TextField e atribuida função ao clicar botão!

--- ModeloDeFormulario.dart
+++ ModeloDeFormulario.dart
@@ -1,36 +1,43 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+
+  final TextEditingController _controllerTicker = TextEditingController();
+  final TextEditingController _controllerCompanhia = TextEditingController();
+  final TextEditingController _controllerCotacao = TextEditingController();
+
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Formulário')),
+        appBar: AppBar(title: const Text('Formulário')),
         body: Column(
           children: <Widget>[
-            const Padding( // adiciona margens personalizaveis;
-              padding: EdgeInsets.all(12.0), //EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16), poderia ser assim também,
+            Padding( // adiciona margens personalizaveis;
+              padding: const EdgeInsets.all(12.0), //EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16), poderia ser assim também,
               child: TextField(
-                style: TextStyle(fontSize: 16.0),
-                decoration: InputDecoration( //Decora o espaço que receberá o texto;
+                controller: _controllerTicker,
+                style: const TextStyle(fontSize: 16.0),
+                decoration: const InputDecoration( //Decora o espaço que receberá o texto;
                   labelText: "Ticker",
                   hintText: 'ABCD3',
                 ),
                 keyboardType: TextInputType.text,
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.all(4.0),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
               child: TextField(
-                style: TextStyle(fontSize: 16.0),
-                decoration: InputDecoration(
+                controller: _controllerCompanhia,
+                style: const TextStyle(fontSize: 16.0),
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(), //Adiciona bordas ao TextField;
                   labelText: "Companhia" ,
                   hintText: 'Empresa tal',
@@ -38,13 +45,15 @@ class MyApp extends StatelessWidget {
                 ),
                 keyboardType: TextInputType.text,
               ),
-            ),const Padding(
-              padding: EdgeInsets.all(4.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
               child: TextField(
-                style: TextStyle(fontSize: 16.0),
-                decoration: InputDecoration(
+                controller: _controllerCotacao,
+                style: const TextStyle(fontSize: 16.0),
+                decoration: const InputDecoration(
                   border: OutlineInputBorder(),
-                  labelText: "Valor",
+                  labelText: "Cotação",
                   hintText: '0.00',
                   icon: Icon(Icons.monetization_on),
                 ),
@@ -52,7 +61,20 @@ class MyApp extends StatelessWidget {
               ),
             ),
             ElevatedButton(
-                onPressed: null,
+                onPressed: (){
+                  final String? ticket = _controllerTicker.text;
+                  final String? companhia = _controllerCompanhia.text;
+                  final double? cotacao = double.tryParse(_controllerCotacao.text);
+
+                  if (ticket!= null && companhia!= null && cotacao!= null){
+                    final Ativo novoAtivo = Ativo(ticket, companhia, cotacao);
+                    // O Intellij sugeriu da forma abaixo ao invés de usar o IF;
+                    //  final Ativo novoAtivo = Ativo(ticket!, companhia!, cotacao!);
+                  debugPrint("$novoAtivo");
+                  } else {
+                    debugPrint('Erro no preenchimento, favor refazer');
+                  }
+                },
                 child: const Text('Confirma', style: TextStyle(color: Colors.white)),
               style: ButtonStyle(
                 backgroundColor: //Mudando a cor de fundo do butão;
@@ -64,5 +86,18 @@ class MyApp extends StatelessWidget {
       ),
 
     );
+  }
+}
+
+class Ativo{
+  final String ticket;
+  final String companhia;
+  final double cotacao;
+
+  Ativo(this.ticket, this.companhia, this.cotacao);
+
+  @override
+  String toString() {
+    return 'Ativo{ticket: $ticket, companhia: $companhia, cotacao: $cotacao}';
   }
 }
