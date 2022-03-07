fbb7d09a28fef61f80ed416a4e924c791e84e01d
marciofreire42 <c135532@gmail.com>
2022-03-07 01:33:40 -0300

    Adicionado chamador do diálogo, melhor tratamendo do AlertDialog!

--- CaixadeDialogo_AlertDialog.dart
+++ CaixadeDialogo_AlertDialog.dart
@@ -1,38 +1,61 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class TransactionAuthDialog extends StatelessWidget {
-  const TransactionAuthDialog({Key? key}) : super(key: key);
+class TransactionAuthDialog extends StatefulWidget {
+  final Function(String password) onConfirm;
+
+  TransactionAuthDialog({required this.onConfirm});
+    // Função requerida na chamada, vai receber um String;
+    // e vai devolver para o chamador do dialog o valor digitado
+
+  @override
+  State<TransactionAuthDialog> createState() => _TransactionAuthDialogState();
+}
+
+class _TransactionAuthDialogState extends State<TransactionAuthDialog> {
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Autenticador!'),
-      content: const TextField(
+      content: TextField(
+        controller: _passwordController,
         obscureText: true,
-        // Para substiruir o texto por *, usado em senhas;
+        // Para substituir o texto por *, usado em senhas;
         maxLength: 4,
         // Indica o tamanho que o campo pode ter;
-        decoration: InputDecoration(
-            labelText: 'Senha', hintText: '0000', border: OutlineInputBorder()),
+        decoration: InputDecoration(border: OutlineInputBorder()),
         textAlign: TextAlign.center,
         keyboardType: TextInputType.number,
-        style: TextStyle(
+        style: const TextStyle(
             fontSize: 60, letterSpacing: 30 // Espaçamento entre letras;
             ),
       ),
       actions: <Widget>[
         TextButton(
             onPressed: () {
-              print('Cancelado');
+              Navigator.pop(context);
             },
             child: const Text('Cancelar')),
         TextButton(
             onPressed: () {
-              print('Continuado');
+              widget.onConfirm(_passwordController.text);
+                // Valor digitado é atribuido na function.
+              Navigator.pop(context);
             },
             child: Text('Continuar'))
       ],
     );
   }
 }
+
+// O chamador da Caixa de Diálogo acontece da seguinte forma:
+showDialog(
+    context: context, 
+    builder: (context){
+        return TransactionAuthDialog(onConfirm: (String password) {
+        print('A senha é $password');
+        },);
+
+// Assim como para chamar outra tela usamos o Navigator, para caixa de diálogo showDialog;
